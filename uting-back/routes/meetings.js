c820c2abb1d1b4593b33901e00271ae318252176
gnupdev <gnupdev@gmail.com>
2021-04-15 02:06:18 +0900

    implement 'createmeeing test' in backend

--- uting-back/routes/meetings.js
+++ uting-back/routes/meetings.js
@@ -2,6 +2,40 @@ var express = require('express');
 var router = express.Router();
 const { Meeting }=require('../model');
 
+
+const fs = require('fs');
+const uuid = require('uuid/v4');
+const AWS = require('aws-sdk');
+/* eslint-enable */
+
+let hostname = '127.0.0.1';
+let port = 8080;
+let protocol = 'http';
+let options = {};
+
+const chime = new AWS.Chime({ region: 'us-east-1' });
+const alternateEndpoint = process.env.ENDPOINT;
+if (alternateEndpoint) {
+  console.log('Using endpoint: ' + alternateEndpoint);
+  chime.createMeeting({ ClientRequestToken: uuid() }, () => {});
+  AWS.NodeHttpClient.sslAgent.options.rejectUnauthorized = false;
+  chime.endpoint = new AWS.Endpoint(alternateEndpoint);
+} else {
+  chime.endpoint = new AWS.Endpoint(
+    'https://service.chime.aws.amazon.com/console'
+  );
+}
+
+const meetingCache = {};
+const attendeeCache = {};
+
+const log = message => {
+  console.log(`${new Date().toISOString()} ${message}`);
+};
+
+const app = process.env.npm_config_app || 'meeting';
+
+
 // GET meetings list 
 router.get('/', async function(req, res, next) {
   Meeting.find({}).then((meetings)=>{ 
@@ -17,12 +51,27 @@ router.get('/:id', async function(req, res, next) {
 });
 
 // POST write one meeting
-router.post('/', function(req, res,next){
+router.post('/', async function(req, res,next){
   const meeting = new Meeting({
     title:req.body.title,
     num:req.body.num,
     status:req.body.status
   });
+
+  const title = meeting.title;
+  const name = "백승수";
+  const region = "us-east-1"
+  
+  if (!meetingCache[title]){
+    meetingCache[title] = await chime
+      .createMeeting({
+        ClientRequestToken: uuid(),
+        MediaRegion: region
+      })
+      .promise();
+      attendeeCache[title] = {};
+  }
+  console.log(meetingCache[title]);
 
   meeting.save((err)=>{
     res.send("방을 생성하였습니다.")
